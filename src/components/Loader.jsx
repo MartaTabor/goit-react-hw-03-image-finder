30f10d417933a1de5d65032130b45beeb675a5fa
Marta Tabor <leeloothefifth@googlemail.com>
2024-04-18 23:26:10 +0100

    loader margin

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,15 @@
 import { Audio } from 'react-loader-spinner';
 
 export const Loader = () => (
-  <Audio
-    height="80"
-    width="80"
-    radius="9"
-    color="green"
-    ariaLabel="three-dots-loading"
-    wrapperStyle
-    wrapperClass
-  />
+  <div className="loader">
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="three-dots-loading"
+      wrapperStyle
+      wrapperClass
+    />
+  </div>
 );
